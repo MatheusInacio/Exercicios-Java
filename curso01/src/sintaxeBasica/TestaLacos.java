74ce0a32aafa9a3c7296c55f3acf70fa883b6a7f
Matheus <Matheus@DESKTOP.local>
2023-07-29 16:56:11 -0300

    "Java JRE e JDK: compile e execute o seu programa" ok

---
+++ curso01/src/sintaxeBasica/TestaLacos.java
@@ -0,0 +1,15 @@
+package sintaxeBasica;
+
+public class TestaLacos {
+	public static void main(String[] args) {
+		System.out.println("Tabuada");
+		for (int multiplicador = 1; multiplicador <= 10; multiplicador++) {
+			System.out.print(multiplicador + ": ");
+			for (int contador = 0; contador <= 10; contador++) {
+				System.out.print(multiplicador * contador);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ curso01/src/sintaxeBasica/TestaVariaveis.java
@@ -0,0 +1,13 @@
+package sintaxeBasica;
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+
+		System.out.println("ola novo teste");
+
+		int idade = 31;
+
+		System.out.println("a idade é " + idade + ", parabéns!");
+	}
+}
